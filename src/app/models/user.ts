93eed6de3aca11329f2e138ab888b029c4b3882c
Victor Afolabi <olubusayo122@gmail.com>
2024-01-30 15:01:06 +0100

    user details page

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -9,11 +9,19 @@ interface Address {
   zipcode: string;
   geo: Geo;
 }
+interface Company{
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
 export default interface User {
   id: number;
   name: string;
   username: string;
   email: string;
   address: Address;
+  phone: number;
+  website: string;
+  company: Company;
 }
 // Now you can use these interfaces in your Angular components or services.
