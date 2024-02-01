4727047b00914b127f0c83422f92c529b6cb25bc
davidakabogu <Akabogudavid@gmail.com>
2024-02-01 16:32:40 +0100

    Revert "Updated User list Design"

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,29 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import User from '../models/user';
 import { UserService } from '../services/user.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule, RouterLink, SearchComponent],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css',
+  styleUrl: './card.component.css'
 })
 export class CardComponent {
   // get users from api and store in a variable users
-   @Input({ required: true })
-  user!: User;
-  // // onSearchTextEntered(searchValue: string) {
-  // //   this.searchText = searchValue;
-  // // }// get users from api and store in a variable users
   users: Observable<User[]> = this.userService.getUsers();
   constructor(private userService: UserService) {}
-  
+  // when clicked by enduser should retrive the user ID
   getUserInfo(id: number) {
-    this.userService.getUserInfoAndEmit(id)
+    this.userService.getUserInfoAndEmit(id);
   }
 }
